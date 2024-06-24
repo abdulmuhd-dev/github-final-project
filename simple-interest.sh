26072b2e1dffbd383a2b065352779f154730914c
Abdulrahman Muhammad <69073155+abdulmuhd-dev@users.noreply.github.com>
2024-06-24 19:16:44 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # flashcode-source.
+   # Abdulrahman Muhammad.
    # Input:
    # p, principal amount
    # t, time period in years
